32019da7b62780f85312385b36b75d31581662a6
Pauline Davis <pauljdavi.t21@btinternet.com>
2022-11-16 14:50:39 +0000

    passing test on degrading twice as fast as per sell by date

--- spec/gildedRose.test.js
+++ spec/gildedRose.test.js
@@ -8,4 +8,14 @@ describe("Gilded Rose", function() {
     expect(items[0].sellIn).toBe(-1);
     expect(items[0].quality).toBe(0);
   });
+
+  it('quality degrades twice as fast after sell by date', function() {
+    const gildedRose = new Shop([new Item('foo', 0, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe('foo');
+    expect(items[0].sellIn).toBe(-1);
+    expect(items[0].quality).toBe(8);
+  });
+    
+  
 });
