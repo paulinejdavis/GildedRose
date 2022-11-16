1f07d0369647de82bafdb070654bdfd16a26ac0e
Pauline Davis <pauljdavi.t21@btinternet.com>
2022-11-16 14:40:36 +0000

    passing test on 'should foo' test

--- spec/gildedRose.test.js
+++ spec/gildedRose.test.js
@@ -1,9 +1,11 @@
-const {Shop, Item} = require("../spec/gilded_rose");
+const {Shop, Item} = require("../lib/gildedRose");
 
 describe("Gilded Rose", function() {
   it("should foo", function() {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+    expect(items[0].name).toBe("foo");
+    expect(items[0].sellIn).toBe(-1);
+    expect(items[0].quality).toBe(0);
   });
 });
